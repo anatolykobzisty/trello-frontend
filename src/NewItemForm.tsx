2c597b7b234f3d4c4502209e4c67acdc537000b7
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2021-08-13 17:10:16 +0300

    Requested Feature - Submit on Enter Press

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -10,9 +10,15 @@ export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState("");
   const inputRef = useFocus();
 
+  const handleAddText = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      onAdd(text);
+    }
+  };
+
   return (
     <NewItemFormContainer>
-      <NewItemInput ref={inputRef} value={text} onChange={(e) => setText(e.target.value)} />
+      <NewItemInput ref={inputRef} value={text} onChange={(e) => setText(e.target.value)} onKeyPress={handleAddText} />
       <NewItemButton onClick={() => onAdd(text)}> Create</NewItemButton>
     </NewItemFormContainer>
   );
