9bbfbfea420b2f96ef1af5e80498d1e324022b3e
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2021-08-13 18:29:54 +0300

    Implement State Management

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,18 @@ import { FC } from "react";
 import { AppContainer } from "./styles";
 
 import { Column } from "./Column";
-import { Card } from "./Card";
 import { AddNewItem } from "./AddNewItem";
 
+import { useAppState } from "./state/AppStateContext";
+
 export const App: FC = () => {
+  const { lists } = useAppState();
+
   return (
     <AppContainer>
-      <Column text="To Do">
-        <Card text="Generate app scaffold" />
-      </Column>
-      <Column text="In Progress">
-        <Card text="Learn Typescript" />
-      </Column>
-      <Column text="Done">
-        <Card text="Begin to use static typing" />
-      </Column>
+      {lists.map((list) => (
+        <Column text={list.text} key={list.id} id={list.id} />
+      ))}
       <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
     </AppContainer>
   );
