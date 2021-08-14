bc35b5aa24d2ab5b60e8189164be67375b8a962a
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2021-08-14 23:25:53 +0300

    Hide The Default Drag Preview

--- src/utils/useItemDrag.ts
+++ src/utils/useItemDrag.ts
@@ -1,4 +1,7 @@
+import { useEffect } from "react";
 import { useDrag } from "react-dnd";
+import { getEmptyImage } from "react-dnd-html5-backend";
+
 import { useAppState } from "../state/AppStateContext";
 import { DragItem } from "../DragItem";
 import { setDraggedItem } from "../state/actions";
@@ -6,7 +9,7 @@ import { setDraggedItem } from "../state/actions";
 export const useItemDrag = (item: DragItem) => {
   const { dispatch } = useAppState();
 
-  const [, drag] = useDrag({
+  const [, drag, preview] = useDrag({
     type: item.type,
     item: () => {
       dispatch(setDraggedItem(item));
@@ -14,6 +17,10 @@ export const useItemDrag = (item: DragItem) => {
     },
     end: () => dispatch(setDraggedItem(null)),
   });
+
+  useEffect(() => {
+    preview(getEmptyImage(), { captureDraggingState: true });
+  }, [preview]);
 
   return { drag };
 };
