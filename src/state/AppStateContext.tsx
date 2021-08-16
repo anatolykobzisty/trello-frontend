4a82b4641c4a836e96c86930885f24f824b05a98
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2021-08-16 10:37:12 +0300

    Use The HOC

--- src/state/AppStateContext.tsx
+++ src/state/AppStateContext.tsx
@@ -1,31 +1,11 @@
-import { createContext, useContext, useEffect, Dispatch, FC } from "react";
+import { createContext, useContext, useEffect, Dispatch } from "react";
 import { useImmerReducer } from "use-immer";
 
 import { appStateReducer, AppState, List, Task } from "./appStateReducer";
 import { Action } from "./actions";
 import { DragItem } from "../DragItem";
 import { save } from "../api";
-
-const appData: AppState = {
-  draggedItem: null,
-  lists: [
-    {
-      id: "0",
-      text: "To Do",
-      tasks: [{ id: "c0", text: "Generate app scaffold" }],
-    },
-    {
-      id: "1",
-      text: "In Progress",
-      tasks: [{ id: "c2", text: "Learn Typescript" }],
-    },
-    {
-      id: "2",
-      text: "Done",
-      tasks: [{ id: "c3", text: "Begin to use static typing" }],
-    },
-  ],
-};
+import { withInitialState } from "../withInitialState";
 
 type AppStateContextProps = {
   draggedItem: DragItem | null;
@@ -36,22 +16,27 @@ type AppStateContextProps = {
 
 const AppStateContext = createContext<AppStateContextProps>({} as AppStateContextProps);
 
-export const AppStateProvider: FC = ({ children }) => {
-  const [state, dispatch] = useImmerReducer(appStateReducer, appData);
+type AppStateProviderProps = {
+  children: React.ReactNode;
+  initialState: AppState;
+};
+
+export const AppStateProvider = withInitialState<AppStateProviderProps>(({ children, initialState }) => {
+  const [state, dispatch] = useImmerReducer(appStateReducer, initialState);
+
+  useEffect(() => {
+    save(state);
+  }, [state]);
 
   const { draggedItem, lists } = state;
   const getTasksByListId = (id: string) => {
     return lists.find((list) => list.id === id)?.tasks || [];
   };
 
-  useEffect(() => {
-    save(state);
-  }, [state]);
-
   return (
     <AppStateContext.Provider value={{ draggedItem, lists, getTasksByListId, dispatch }}>{children}</AppStateContext.Provider>
   );
-};
+});
 
 export const useAppState = () => {
   return useContext(AppStateContext);
